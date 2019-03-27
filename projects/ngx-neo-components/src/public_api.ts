7a1f8ff2847194837a876d6e154331ea3c423c64
Brenda Dilschneider <bdilschneider@team.neocomplexx.com>
2019-03-27 16:55:44 -0300

    se exportan las animaciones

--- projects/ngx-neo-components/src/public_api.ts
+++ projects/ngx-neo-components/src/public_api.ts
@@ -9,3 +9,4 @@ export * from './lib/footer/footer.component';
 export * from './lib/sidebar/sidebar.component';
 export * from './lib/mobile-sidebar/mobile-sidebar.component';
 export * from './lib/ngx-neo-components.module';
+export * from './lib/shared/animations/keyframes';
