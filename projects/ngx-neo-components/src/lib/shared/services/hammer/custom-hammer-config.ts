0c164b9247e042be45d7d1805f0bef9b8f4328bd
Brenda Dilschneider <bdilschneider@team.neocomplexx.com>
2019-03-28 09:14:37 -0300

    agregado hammer para mobile gestures

---
+++ projects/ngx-neo-components/src/lib/shared/services/hammer/custom-hammer-config.ts
@@ -0,0 +1,8 @@
+import { HammerGestureConfig, HAMMER_GESTURE_CONFIG } from '@angular/platform-browser';
+
+export class CustomHammerConfig extends HammerGestureConfig  {
+    overrides = <any>{
+        'pinch': { enable: false },
+        'rotate': { enable: false }
+    };
+}
