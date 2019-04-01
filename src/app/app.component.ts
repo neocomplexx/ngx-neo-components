a2a862e0f21ecd45a261a7f30a3a46066d941084
Brenda Dilschneider <bdilschneider@team.neocomplexx.com>
2019-04-01 16:56:34 -0300

    undo component - defined partially

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -49,6 +49,8 @@ export class AppComponent {
   public showUndo: boolean;
   public actionText: string;
   public undoMessage: string;
+  public notificacionObtenida: Notification;
+  public notificationSwipeRight: boolean;
 
   constructor(private headerService: HeaderService, private mobileSidebarService: MobileSidebarService) {
 
@@ -115,7 +117,15 @@ export class AppComponent {
       this.showUndo = true;
       this.undoMessage = '1 archivada';
       notif.show = false;
+      this.notificacionObtenida = notif;
+      this.notificationSwipeRight = true;
       }, 500);
+
+      // setTimeout(() => {
+      //   console.log('Timeout para el mensaje undo');
+      //   this.showUndo = false;
+      //   this.finishActionSwipeRight();
+      // }, 5000);
   }
 
   public onNotifySwipeLeft(event, notif: Notification) {
@@ -124,20 +134,46 @@ export class AppComponent {
       this.showUndo = true;
       this.undoMessage = '1 eliminada';
       notif.show = false;
+      this.notificacionObtenida = notif;
+      this.notificationSwipeRight = false;
+      console.log('Time out show undo');
       }, 500);
+
+    // setTimeout(() => {
+    //   console.log('Timeout para el mensaje undo');
+    //   this.showUndo = false;
+    //   this.finishActionSwipeLeft();
+    // }, 5000);
   }
 
   public onUndo(event) {
     console.log(event, 'UNDO');
   //  this.showUndo = false;
+   if (this.notificationSwipeRight) {
+     this.undoSwipeRight ();
+   } else {
+     this.undoSwipeLeft();
+   }
   }
 
   // Estos métodos harían las llamadas al backend correspondientes
-  public finishActionSwipeRight() {}
-  public finishActionSwipeLeft() {}
+  public finishActionSwipeRight() {
+    window.alert('El sistema informa que se terminó la accion del swipe right.');
+  }
+  public finishActionSwipeLeft() {
+    window.alert('El sistema informa que se terminó la accion del swipe left.');
+  }
 
-  public undoSwipeRight() {}
-  public undoSwipeLeft() {}
+  public undoSwipeRight() {
+   // window.alert('Deshago swipe right');
+    this.notificacionObtenida.show = true;
+    this.showUndo = false;
+  }
+  public undoSwipeLeft() {
+   // window.alert('Deshago swipe left');
+    this.notificacionObtenida.show = true;
+    this.showUndo = false;
+  }
 
   @HostListener('swipeleft', ['$event'])
   public hideSidebar($event) {
