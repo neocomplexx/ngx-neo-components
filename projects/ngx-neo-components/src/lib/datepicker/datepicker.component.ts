192cf9fb12466bad9905c98e94d4ac619e2f2f46
Juan Ignacio Cangelosi <jcangelosi@Mac-mini-de-Mauro.local>
2019-05-03 17:13:13 -0300

    datepicker

--- projects/ngx-neo-components/src/lib/datepicker/datepicker.component.ts
+++ projects/ngx-neo-components/src/lib/datepicker/datepicker.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
-import {Platform} from '@angular/cdk/platform';
+import { Platform } from '@angular/cdk/platform';
 import { NgbDate } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
@@ -8,10 +8,24 @@ import { NgbDate } from '@ng-bootstrap/ng-bootstrap';
 })
 export class DatepickerComponent {
 
-    @Input() date: Date;
+    private _date: Date;
+    @Input() set date(value: Date) {
+        this._date = value;
+        this._ngbdate = new NgbDate(value.getFullYear(), value.getMonth() + 1, value.getDate());
+    };
+    get date() {
+        return this._date;
+    }
     @Output() dateChange: EventEmitter<Date> = new EventEmitter<Date>();
 
-    @Input() ngbdate: NgbDate;
+    private _ngbdate: NgbDate;
+    @Input() set ngbdate(value: NgbDate) {
+        this._ngbdate = value;
+        this._date = new Date(value.year, value.month - 1, value.day);
+    };
+    get ngbdate(): NgbDate {
+        return this._ngbdate;
+    }
     @Output() ngbdateChange: EventEmitter<NgbDate> = new EventEmitter<NgbDate>();
 
     public isMobile = false;
@@ -25,12 +39,18 @@ export class DatepickerComponent {
         this.dateChange.emit(dateModel);
     }
 
-    public get dateModel(): Date {
-        return this.date;
+    public get dateModel(): string {
+        if (this.date) {
+            return this.date.toISOString().substring(0, 10);
+        } else {
+            return undefined;
+        }
     }
-    public set dateModel(value: Date) {
-        const ngbModel = new NgbDate(value.getFullYear(), value.getMonth() + 1, value.getDate());
-        this.dateChange.emit(value);
+    public set dateModel(value: string) {
+        if (value.length === 10) { value += 'T03:00:00Z'; }
+        const valueDate = new Date(value);
+        const ngbModel = new NgbDate(valueDate.getFullYear(), valueDate.getMonth() + 1, valueDate.getDate());
+        this.dateChange.emit(valueDate);
         this.ngbdateChange.emit(ngbModel);
     }
 
