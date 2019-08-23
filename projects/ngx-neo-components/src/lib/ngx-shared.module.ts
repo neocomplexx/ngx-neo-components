6a637531ec1eaa9d26c0376570f28da2a42a451a
Brenda Dilschneider <bdilschneider@team.neocomplexx.com>
2019-08-23 14:33:28 -0300

    sidebar changes

--- projects/ngx-neo-components/src/lib/ngx-shared.module.ts
+++ projects/ngx-neo-components/src/lib/ngx-shared.module.ts
@@ -2,9 +2,12 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import { NgModule } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatListModule } from '@angular/material/list';
+import {MatMenuModule} from '@angular/material/menu';
 
 @NgModule({
-    imports: [MatButtonModule, MatCheckboxModule, MatToolbarModule],
-    exports: [MatButtonModule, MatCheckboxModule, MatToolbarModule],
+    imports: [MatButtonModule, MatCheckboxModule, MatToolbarModule, MatCardModule, MatListModule, MatMenuModule],
+    exports: [MatButtonModule, MatCheckboxModule, MatToolbarModule, MatCardModule, MatListModule, MatMenuModule],
 })
 export class NgxSharedModule { }
