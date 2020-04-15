be32b3bc98ddf250d8c8cfc4e15254596fe0ab7b
sebalr <larrieu.sebastian@gmail.com>
2020-04-15 09:26:53 -0300

    new version. public close sidebar

--- projects/ngx-neo-components-mat/src/lib/sidebar/sidebar.service.ts
+++ projects/ngx-neo-components-mat/src/lib/sidebar/sidebar.service.ts
@@ -1,12 +1,34 @@
-import { Injectable } from '@angular/core';
-import { Subject } from 'rxjs';
+import { Injectable, OnDestroy } from '@angular/core';
+import { Subject, Subscription, BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
-export class SidebarService {
+export class SidebarService implements OnDestroy {
 
   public toggleSidebar: Subject<any> = new Subject();
 
-  constructor() { }
+  private openSate = new BehaviorSubject<boolean>(true);
+  public openSate$ = this.openSate.asObservable();
+
+  private subs = new Subscription();
+
+  constructor() {
+    this.subs.add(this.toggleSidebar.subscribe(() => {
+      const state = this.openSate.getValue();
+      this.openSate.next(!state);
+    }));
+  }
+
+  public closeSidebar() {
+    this.openSate.next(false);
+  }
+
+  public openSidebar() {
+    this.openSate.next(true);
+  }
+
+  ngOnDestroy() {
+    this.subs.unsubscribe();
+  }
 }
